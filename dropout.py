03c1f4db1ceef5e55a908a80ee6938685c1bcf86
saptanath <saptnath1@gmail.com>
2022-02-22 19:56:33 -0500

    added plotting

--- dropout.py
+++ dropout.py
@@ -12,6 +12,10 @@ import torch.utils.data as data
 from torchvision import datasets, transforms
 import torch.optim as optim
 from torch.autograd import Variable
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
+import numpy as np
 
 
 class Gaussian(nn.Module):
@@ -53,10 +57,11 @@ class Network(nn.Module):
         
     
 class Train_Test(object):
-    def __init__(self, n_epochs = 100, lr = 0.01, method_='standard', p=0.5):
+    def __init__(self, n_epochs = 10, lr = 0.01, method_='standard', p=0.5):
         self.n_epochs = n_epochs
         self.p_val = p
         self.dim = 28*28
+        self.meth = method_
         
         self.train_load = data.DataLoader(
             datasets.MNIST('../data', train=True, download=True, transform=transforms.ToTensor()), 
@@ -69,38 +74,94 @@ class Train_Test(object):
         self.net = Network(method=method_, p=self.p_val)
         self.loss = nn.CrossEntropyLoss()
         self.optim = optim.SGD(self.net.parameters(), lr=lr)
+        self.ls_tr = []
+        self.cr_tr = []
+        self.ls_ts = []
+        self.cr_ts = []
         
     def train(self):
         self.net.train()
         for epoch in range(self.n_epochs):
             epoch += 1
             total_loss = 0
+            correct = 0
+            len_ = 0
             for data_, label in self.train_load:
                 data_, label = Variable(data_).view(-1, self.dim), Variable(label)
                 output = self.net(data_)
+                _, predicted = torch.max(output.data, 1)
                 loss_ = self.loss(output, label)
                 self.optim.zero_grad()
                 loss_.backward()
                 self.optim.step()
                 total_loss += float(loss_.data)
+                correct += (predicted.cpu() == label).sum()
+                len_ += label.size(0)
             total_loss /= len(self.train_load.dataset)
+            self.ls_tr.append(total_loss)
+            self.cr_tr.append(correct/len_)
             print('Epoch {} | loss: {:.6f}'.format(epoch, total_loss))
+        self.plot_tool(self.ls_tr, self.cr_tr, self.p_val, self.meth, 'train')
             
     def test(self):
         self.net.eval()
-        correct = 0
-        len_ = 0
-        for data_, label in enumerate(self.test_load):
-            data_, label = Variable(data_).view(-1, self.dim), Variable(label)
-            output = self.net(data_)
-            _, predicted = torch.max(output.data, 1)
-            correct += (predicted.cpu() == label).sum()
-            len_ += label.size(0)
-        perc = 100*(correct/len_)
-        print('Accuracy: {:.4f}%'.format(perc))
+        for epoch in range(self.n_epochs):
+            epoch += 1
+            correct = 0
+            len_ = 0
+            total_loss = 0
+            for data_, label in enumerate(self.test_load):
+                data_, label = Variable(data_).view(-1, self.dim), Variable(label)
+                output = self.net(data_)
+                _, predicted = torch.max(output.data, 1)
+                loss_ = self.loss(output, label)
+                correct += (predicted.cpu() == label).sum()
+                total_loss += float(loss_.data)
+                len_ += label.size(0)
+            perc = 100*(correct/len_)
+            total_loss /= len(self.test_load.dataset)
+            self.ls_ts.append(total_loss)
+            self.cr_ts.append(correct/len_)
+            print('Accuracy: {:.4f}%'.format(perc))
+        self.plot_tool(self.ls_ts, self.cr_ts, self.p_val, self.meth, 'test')
         
-standard = Train_Test(method_='gaussian')
-standard.train()
-standard.test()
+    def plot_tool(self, ls, cr, p_val, met, run):
+        fig = plt.figure(figsize=(20, 10))
+        plt.rc('font', family='serif')
+        plt.xlabel('Epochs')
+        
+        ax = fig.gca()
+        ax.xaxis.set_major_formatter(tick.FormatStrFormatter('%0g'))
+        ax.xaxis.set_ticks(np.arange(0, self.n_epochs+5, 10))
+        
+        ax_sec = ax.twinx()
+        ax.set_autoscaley_on(False)
+        ax_sec.set_autoscaley_on(False)
+        
+        ax.plot(ls, linestyle='--', color='b', label='Loss') 
+        ax_sec.plot(cr, linestyle='_.', color='m', label='accuracy')
+        ax.set_ylabel('Loss')
+        ax_sec.set_ylabel('Accuracy')
+        ax.set_ylim(0, max(ls))
+        ax_sec.set_ylim(0, max(cr))
+        
+        plt.title('P value: {}, method: {}, {}'.format(p_val, met, run))
+        plt.grid(True)
+        plt.tight_layout()
+        
+        plt.savefig('figures/Run_{}_pval_{}_{}.pdf'.format(run, p_val, met), format="pdf")
+        plt.show(block=True)
+        plt.close(fig)
+        
+## main function to run ##
+
+for p_ in np.arange(0.0, 1.1, 0.3):
+    standard = Train_Test(method_='standard', p=p_)
+    standard.train()
+    standard.test()
+for p_ in np.arange(0.0, 1.1, 0.3):
+    gauss = Train_Test(method_='gaussian', p=p_)
+    gauss.train()
+    gauss.test()
             
         
